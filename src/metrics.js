4aa6666ae3c6bdf44d6884bc61d01785987a8a8c
Arman D <adrismir@gmail.com>
2024-09-18 16:42:27 -0700

    Add /metrics endpoint for Prometheus

---
+++ src/metrics.js
@@ -0,0 +1,32 @@
+const client = require('prom-client')
+const register = new client.Registry()
+
+const totalRequests = new client.Counter({
+	name: 'gpt_broker_requests_total',
+	help: 'Number of requests made to gpt-broker',
+	labelNames: ['method', 'status_code'],
+})
+
+const successfulRequests = new client.Counter({
+	name: 'gpt_broker_valid_requests',
+	help: 'The number of requests that passed security and queried GPT',
+	labelNames: ['method', 'status_code']
+})
+
+const failedRequests = new client.Counter({
+	name: 'gpt_broker_failed_requests',
+	help: 'The number of requests that failed security checks',
+	labelNames: ['method', 'status_code']
+})
+
+register.registerMetric(totalRequests);
+register.registerMetric(successfulRequests);
+register.registerMetric(failedRequests);
+
+module.exports = {
+	register,
+	totalRequests,
+	successfulRequests,
+	failedRequests,
+}
+
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const Router = require('koa-router')
+const { validateUser, CheckRateLimit } = require('./middleware')
 const { generateResponse } = require('./utils')
+const { register } = require('./metrics')
 
 const router = new Router();
 
@@ -7,18 +9,18 @@ const router = new Router();
  * Expects a param 'prompt' for GPT Generation
  * @returns - GPT's response in the message body
  */
-router.get('/simple-gpt-4o-mini-complete', async ctx => {
+router.get('/simple-gpt-4o-mini-complete', validateUser, CheckRateLimit, async ctx => {
 	const prompt = ctx.query.prompt;
 
 	if (!prompt) {
 		ctx.status = 400;
-		ctx.body = { error: "Missing required 'prompt' query parameter" };
+		ctx.body = { error: "Missing required 'prompt' query parameter", message: null };
 		return;
 	}
 
 	if (typeof prompt !== 'string') {
 		ctx.status = 400;
-		ctx.body = { error: "'prompt' must be a string" };
+		ctx.body = { error: "'prompt' must be a string", message: null };
 		return;
 	}
 
@@ -37,11 +39,11 @@ router.get('/simple-gpt-4o-mini-complete', async ctx => {
  * ie. [ { role: 'system', content: "Write me a poem" }, ... ]
  * @returns - a response with GPT's response in the body
  */
-router.post('/advanced-gpt-4o-mini-complete', async ctx => {
+router.post('/advanced-gpt-4o-mini-complete', validateUser, CheckRateLimit, async ctx => {
 	const messages = ctx.request.body;
 	if (!messages) {
 		ctx.status = 400;
-		ctx.body = { error: "Missing required 'messages' in body of request" };
+		ctx.body = { error: "Missing required 'messages' in body of request", message: null };
 		return;
 	}
 	try {
@@ -49,8 +51,14 @@ router.post('/advanced-gpt-4o-mini-complete', async ctx => {
 		ctx.body = response;
 	} catch (error) {
 		ctx.status = 500;
-		ctx.body = { error: `GPT Threw an error - ${error.message}`};
+		ctx.body = { error: `GPT Threw an error - ${error.message}`, message: null};
 	}
+})
+
+
+router.get('/metrics', async ctx => {
+	ctx.type = 'text/plain';
+	ctx.body = await register.metrics();
 })
 
 module.exports = { router };
